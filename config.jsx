13dba398d21a4dc0d0e71eed56086fa4c12248d4
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-03-19 23:37:33 +0700

    homeAPI

--- config.jsx
+++ config.jsx
@@ -1,3 +1,3 @@
-// export const homeAPI = 'http://139.180.146.179:9999'
-export const homeAPI = 'http://localhost:3001'
-export const feAPI = 'http://localhost:3000'  
+export const homeAPI = 'https://be.xesuzukicantho.com'
+// export const homeAPI = 'http://localhost:9999'
+// export const feAPI = 'http://localhost:3000'  
