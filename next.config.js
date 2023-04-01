2266447f38011f2c229ac61e2469b17d1e784601
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-04-01 20:08:13 +0700

    config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-
 module.exports = async (phase, { defaultConfig }) => {
   /**
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
-    /* config options here */
-    devPagesManifest: true,
     reactStrictMode: true,
-  }
-  return nextConfig
-}
+    async rewrites() {
+      return [
+        {
+          source: "/products/:id",
+          destination: "/products/[id]",
+        },
+      ];
+    },
+  };
+  return nextConfig;
+};
