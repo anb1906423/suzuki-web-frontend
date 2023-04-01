d306009d94866ad1831a68b4852f3dee62ffab31
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-04-01 21:09:34 +0700

    config file

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Script from 'next/script'
-import LayoutComponent from '../components/Layout'
 import { useEffect } from 'react'
+import dynamic from 'next/dynamic';
+
+const Layout = dynamic(() => import('../components/Layout'), {
+  ssr: false,
+});
 
 export default function MyApp({ Component, pageProps }) {
 
@@ -10,13 +14,13 @@ export default function MyApp({ Component, pageProps }) {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
   return (
-    <LayoutComponent>
+    <Layout>
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossOrigin="anonymous"
       />
       <Component {...pageProps} />
-    </LayoutComponent>
+    </Layout>
   )
 }
