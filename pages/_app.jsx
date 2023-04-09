8e6666eab3931e9c33a505dd33ec01ebe3e6ab62
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-04-09 22:32:29 +0700

    favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Script from 'next/script'
 import { useEffect } from 'react'
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 const Layout = dynamic(() => import('../components/Layout'), {
   ssr: false,
@@ -20,6 +21,9 @@ export default function MyApp({ Component, pageProps }) {
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossOrigin="anonymous"
       />
+      <Head>
+        <link rel="icon" href="https://suzuki.com.vn/templates/suzukivietnam/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
